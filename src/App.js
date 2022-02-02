b1f2260b3f14d61897ad0f2746d3e39742f53ab5
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-02 15:42:49 -0800

    array of goblins displays

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ function App() {
         
       />
       <GoblinList 
-        goblins={[]} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array 
+        goblins={goblins} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array 
         handleDeleteGoblin={handleDeleteGoblin} // note that the goblin list has access to the ability to delete
       />
     </div>
